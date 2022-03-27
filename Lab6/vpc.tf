cd5bc62d7de5927b1461ccc6c316b1d68922a0ec
Olah <bodunrin.olah@gmail.com>
2022-03-27 17:00:35 -0400

    privateKey removal

---
+++ Lab6/vpc.tf
@@ -0,0 +1,30 @@
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "learntf-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+  
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+    Description = "VPC_From_Terraform_module"
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+  profile = var.profile
+}
+
+resource "aws_instance" "web_server" {
+  ami = "ami-6871a115"
+  instance_type = "t2.micro"
+  subnet_id = "${module.vpc.public_subnets[0]}"
+}
